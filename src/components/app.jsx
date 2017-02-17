439845692897ae362cb558631b2c47b114e4719e
Panos Voudouris <panosvoudouris@gmail.com>
2017-02-17 16:12:42 +0000

    Sample app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Header, Footer } from '@buildit/ds-demo-lib';
 
-const App = (props) => {
-    return (
-        <div>
-            {props.children}
-        </div>
-    )
+const App = () => {
+  return (
+    <div>
+      <Header>This is a header</Header>
+      This is my app
+      <Footer>This is a footer</Footer>
+    </div>
+  )
 };
 
 export default App;
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('ds-app'));
+const element = document.getElementById('ds-app');
+
+ReactDOM.render(<App />, element);
