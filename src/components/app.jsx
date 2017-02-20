a6e48e52540eaddf18012bf9885304051885c638
Panos Voudouris <panosvoudouris@gmail.com>
2017-02-20 14:56:50 +0000

    Data panel

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Header, Footer } from '@buildit/ds-demo-lib';
 import './styles.less';
+import Panel from './panel';
+
+const data = [
+
+]
 
 const App = () => {
   return (
     <div className="app1">
       <Header>This is the Header for App 1</Header>
-      This is my app
+      <Panel />
       <Footer>The footer for App 1</Footer>
     </div>
   )
---
+++ src/components/panel/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import moment from 'moment';
+import './style.less';
+
+const data = [];
+
+function generateData(data) {
+  let amount = 1000;
+  let d = moment();
+
+  for (let i = 0; i < 50; i++) {
+    data[i] = [ d.subtract(1, 'days').format('D/M/Y'), 0, 0 ];
+  }
+
+  console.log(data);
+}
+
+const Panel = () => {
+  generateData(data);
+  const table = data.map(item => (
+      <tr>
+        <td>{item[0]}</td>
+        <td>{item[1]}</td>
+        <td>{item[2]}</td>
+      </tr>
+    ));
+
+  return (
+    <div className="app-panel">
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Amount</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table}
+        </tbody>
+      </table>
+    </div>
+  )
+};
+
+export default Panel;
